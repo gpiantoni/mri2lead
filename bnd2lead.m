cceecccfe7ca1c0a62d90c86554a0edf552b9cc2
Gio <github@gpiantoni.com>
2012-03-02 09:38:59 +0100

    renamed mri2vol and vol2lead into mri2bnd and bnd2lead

---
+++ bnd2lead.m
@@ -0,0 +1,118 @@
+function bnd2lead(cfg, subj)
+%VOL2LEAD create leadfield, based on volume
+%
+% CFG
+%  .data: name of projects/PROJNAME/subjects/
+%  .rec: name of the recordings (part of the structrual filename)
+%  .vol.mod: name to be used in projects/PROJNAME/subjects/0001/VOLMOD/
+%  .vol.cond: name to be used in projects/PROJNAME/subjects/0001/VOLMOD/VOLCONDNAME/
+%  
+%  .bnd2lead.method: method for head model ('bem_dipoli' 'bem_openmeeg' 'bemcp')
+%  .bnd2lead.conductivity: conductivity of tissues ([0.3300 0.0042 0.3300])
+%
+% Part of MRI2LEAD
+% see also CPMRI, MRI2BND, BND2LEAD
+
+% load /data1/toolbox/elecloc/easycap_61_FT.mat elec 
+% sens = [];
+% sens.elecpos = [elec.pnt; 0 0 0; 0 0 0; 0 0 0];
+% sens.chanpos = [elec.pnt; 0 0 0; 0 0 0; 0 0 0];
+% sens.label = elec.label;
+% sens.unit = 'mm';
+%   
+% cfg = [];
+% cfg.template.vol = vol;
+% cfg.individual.elec = sens;
+% [cfg] = ft_interactiverealign(cfg)
+
+%---------------------------%
+%-start log
+output = sprintf('(p%02.f) %s started at %s on %s\n', ...
+  subj, mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'));
+tic_t = tic;
+%---------------------------%
+
+%---------------------------%
+%-dir and files
+mdir = sprintf('%s%04.f/%s/%s/', cfg.data, subj, cfg.vol.mod, cfg.vol.cond); % mridata dir
+mfile = sprintf('%s_%04.f_%s_%s', cfg.rec, subj, cfg.vol.mod, cfg.vol.cond); % mridata
+
+bndfile = [mdir mfile '_bnd'];
+volfile = [mdir mfile '_vol' cfg.mri2bnd.method(4:end)];
+leamfile = [mdir mfile '_lead' cfg.mri2vol.method(4:end)];
+elecfile = [mdir mfile '_elec'];
+%---------------------------%
+
+%-------------------------------------%
+%-load vol
+load(volfile, 'vol')
+
+%-----------------%
+%-headmodel
+cfg3  = [];
+cfg3.method = cfg.bnd2lead.method;
+cfg3.conductivity = cfg.bnd2lead.conductivity;
+vol = ft_prepare_headmodel(cfg3, bnd);
+save(volfile, 'vol')
+%-----------------%
+%-------------------------------------%
+
+%-------------------------------------%
+%-electrodes and leadfield
+if isfield(vol, 'mat')
+  
+  %-----------------%
+  %-elec
+  elec = ft_read_sens(cfg.vol2lead.sensfile);
+  elec.label = upper(elec.label);
+  elec = ft_convert_units(elec, 'mm');
+  
+  %-------%
+  %-simple transformation (based on visual realignment)
+  elec.chanpos = warp_apply(cfg.vol2lead.elecM, elec.chanpos);
+  elec.elecpos = warp_apply(cfg.vol2lead.elecM, elec.elecpos);
+  %-------%
+  
+  [vol, elec] = ft_prepare_vol_sens(vol, elec);
+  save(elecfile, 'elec')
+  %-----------------%
+  
+  %-----------------%
+  %-prepare leadfield
+  cfg4 = [];
+  cfg4.elec = elec;
+  cfg4.vol = vol;
+  cfg4.grid.xgrid =  -70:10:70;
+  cfg4.grid.ygrid = -110:10:80;
+  cfg4.grid.zgrid =  -60:10:90;
+  cfg4.inwardshift = 1; % to avoid dipoles on the border of bnd(3), which are very instable
+  cfg4.grid.tight = 'no';
+  cfg4.feedback = 'none';
+  lead = ft_prepare_leadfield(cfg4, []);
+  save(leamfile, 'lead')
+  %-----------------%
+  
+else
+  
+  %-----------------%
+  output = sprintf('%sft_prepare_headmodel could not create a head model!\n', output);
+  %-----------------%
+  
+end
+%-------------------------------------%
+
+%---------------------------%
+%-end log
+toc_t = toc(tic_t);
+outtmp = sprintf('(p%02.f) %s ended at %s on %s after %s\n\n', ...
+  subj, mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'), ...
+  datestr( datenum(0, 0, 0, 0, 0, toc_t), 'HH:MM:SS'));
+output = [output outtmp];
+
+%-----------------%
+fprintf(output)
+fid = fopen([cfg.log '.txt'], 'a');
+fwrite(fid, output);
+fclose(fid);
+%-----------------%
+%---------------------------%
---
+++ mri2bnd.m
@@ -0,0 +1,126 @@
+function mri2bnd(cfg, subj)
+%MRI2BND create volume, based on MRI
+%
+% CFG
+%  .data: name of projects/PROJNAME/subjects/
+%  .rec: name of the recordings (part of the structrual filename)
+%  .vol.mod: name to be used in projects/PROJNAME/subjects/0001/VOLMOD/
+%  .vol.cond: name to be used in projects/PROJNAME/subjects/0001/VOLMOD/VOLCONDNAME/
+%
+%  .mri2bnd.tpmthreshold: threshold for segmentation (1 default, 0.05 pretty sensitive)
+%  .mri2bnd.numvertices: number of vertices ([2500 1200 1000])
+%  .mri2bnd.smoothbnd: smoothing for mesh
+%  .mri2bnd.threshbnd: threshold for mesh
+%
+% Part of MRI2LEAD
+% see also CPMRI, MRI2BND, BND2LEAD
+
+%---------------------------%
+%-start log
+output = sprintf('(p%02.f) %s started at %s on %s\n', ...
+  subj, mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'));
+tic_t = tic;
+%---------------------------%
+
+%---------------------------%
+%-dir and files
+mdir = sprintf('%s%04.f/%s/%s/', cfg.data, subj, cfg.vol.mod, cfg.vol.cond); % mridata dir
+mfile = sprintf('%s_%04.f_%s_%s', cfg.rec, subj, cfg.vol.mod, cfg.vol.cond); % mridata
+ext = '.nii.gz';
+
+mrifile = [mdir mfile '_' cfg.normalize ext];
+bndfile = [mdir mfile '_bnd'];
+%---------------------------%
+
+%-------------------------------------%
+%-read and prepare mri
+%-----------------%
+%-read
+mri = ft_read_mri(mrifile);
+%-----------------%
+ 
+%-----------------%
+%-segmenting the volume, Tissue Probability Maps
+cfg1 = [];
+cfg1.threshold  = [];
+cfg1.output = 'tpm';
+cfg1.coordsys = 'spm';
+tpm = ft_volumesegment(cfg1, mri);
+tpm.anatomy = mri.anatomy;
+%-----------------%
+
+%-----------------%
+%-segmenting the volume
+cfg1 = [];
+cfg1.threshold  = cfg.mri2bnd.tpmthreshold;
+cfg1.output = 'scalp';
+cfg1.coordsys = 'spm';
+segscalp = ft_volumesegment(cfg1, tpm);
+
+cfg1 = [];
+cfg1.threshold  = [];
+cfg1.output = {'skull' 'brain'};
+cfg1.coordsys = 'spm';
+segment = ft_volumesegment(cfg1, tpm);
+segment.scalp = segscalp.scalp;
+
+clear segscalp
+%-----------------%
+%-------------------------------------%
+
+%-------------------------------------%
+%-mesh and headmodel
+M = segment.transform;
+
+%-----------------%
+%-prepare mesh for skull and brain (easy)
+cfg2 = [];
+cfg2.tissue = {'skull', 'brain'};
+cfg2.numvertices = cfg.mri2bnd.numvertices(2:3);
+cfg2.transform = M;
+bnd = ft_prepare_mesh_new(cfg2, segment);
+%-----------------%
+
+%-----------------%
+%-prepare mesh for scalp (we need to be more liberal with threshold, bc of
+%poor quality of MRI)
+if ~isempty(cfg.mri2bnd.smoothbnd)
+  %-------%
+  %-add padding and fix M matrix by including the padding as well
+  segment.scalp = double(padarray(segment.scalp, [1 1 1] * cfg.mri2lead.smoothbnd, 'both'));
+  M(1:3,4) = M(1:3,4) - cfg.mri2lead.smoothbnd/2 * diag(M(1:3,1:3)); %TODO: check this
+  %-------%
+end
+
+cfg2 = [];
+cfg2.tissue = {'scalp'};
+cfg2.numvertices = cfg.mri2bnd.numvertices(1);
+cfg2.thresholdseg = cfg.mri2bnd.threshbnd;
+cfg2.smoothseg = cfg.mri2bnd.smoothbnd;
+cfg2.transform = M;
+scalp = ft_prepare_mesh_new(cfg2, segment);
+%-----------------%
+
+%-----------------%
+%-combine scalp and bnd
+bnd = [scalp bnd];
+
+save(bndfile, 'bnd')
+%-----------------%
+%-------------------------------------%
+
+%---------------------------%
+%-end log
+toc_t = toc(tic_t);
+outtmp = sprintf('(p%02.f) %s ended at %s on %s after %s\n\n', ...
+  subj, mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'), ...
+  datestr( datenum(0, 0, 0, 0, 0, toc_t), 'HH:MM:SS'));
+output = [output outtmp];
+
+%-----------------%
+fprintf(output)
+fid = fopen([cfg.log '.txt'], 'a');
+fwrite(fid, output);
+fclose(fid);
+%-----------------%
+%---------------------------%
