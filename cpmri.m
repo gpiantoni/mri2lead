96aeb6011e96ae899a76b788feb6a66f9df74611
Gio <github@gpiantoni.com>
2012-03-02 09:38:12 +0100

    added comments to cpmri

--- cpmri.m
+++ cpmri.m
@@ -1,49 +1,49 @@
 function cpmri(cfg, subj)
-%CPMRI copy MRI images
+%CPMRI copy MRI images into subject-folder and normalize them
+%
+% CFG
+%  .rec: name of the recordings (part of the structrual filename)
+%  .recs: name of recordings/RECNAME/subjects/
+%  .data: name of projects/PROJNAME/subjects/
+%  .vol.mod: name to be used in projects/PROJNAME/subjects/0001/VOLMOD/
+%  .vol.cond: name to be used in projects/PROJNAME/subjects/0001/VOLMOD/VOLCONDNAME/
+%
+%  .normalize: use 'spm' or 'flirt' for spatial normalization to MNI (or ''  for no normalization)
+%  .smri: directory to copy all the structrual data to (can be empty)
+%
+% Part of MRI2LEAD
+% see also CPMRI, MRI2VOL, VOL2LEAD
+
+% to average all the heads together
 % fslmaths gosd_svui_0001_smri_t1_brain.nii.gz -add gosd_svui_0002_smri_t1_brain.nii.gz -add gosd_svui_0003_smri_t1_brain.nii.gz -add gosd_svui_0004_smri_t1_brain.nii.gz -add gosd_svui_0006_smri_t1_brain.nii.gz -add gosd_svui_0006_smri_t1_brain.nii.gz -add gosd_svui_0007_smri_t1_brain.nii.gz -add gosd_svui_0008_smri_t1_brain.nii.gz -add gosd_svui_0009_smri_t1_brain.nii.gz -add gosd_svui_0010_smri_t1_brain.nii.gz -add gosd_svui_0011_smri_t1_brain.nii.gz -add gosd_svui_0012_smri_t1_brain.nii.gz -add gosd_svui_0013_smri_t1_brain.nii.gz -add gosd_svui_0015_smri_t1_brain.nii.gz -div 13 gosd_svui_avg_smri_t1_brain
-
-mversion = 5;
-%05 12/02/14 renamed to cpmri
-%04 11/11/10 spm implemented as well, it works better (faster)
-%03 11/11/10 use fsl
-%02 11/11/10 away from swdti, into source
-%01 11/10/11 created
-
-%-----------------%
-%-input
-if nargin == 1
-  subj = cfg.subj;
-end
-%-----------------%
 
 %---------------------------%
 %-start log
-output = sprintf('(p%02.f) %s (v%02.f) started at %s on %s\n', ...
-  subj, mfilename,  mversion, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'));
+output = sprintf('(p%02.f) %s started at %s on %s\n', ...
+  subj, mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'));
 tic_t = tic;
 %---------------------------%
 
 %---------------------------%
 %-dir and files
-addpath /usr/local/toolbox/fieldtrip/external/spm8/ % <- don't add normal spm8, but this simpler version
+ft_hastoolbox('spm8', 2)
 
-rdir = sprintf('%s%04.f/%s/%s/', cfg.recs, subj, cfg.mod, cfg.rawd); % recording
-ddir = sprintf('%s%04.f/%s/%s/', cfg.data, subj, cfg.mod, cfg.cond); % data
-if ~isdir(ddir); mkdir(ddir); end
+rdir = sprintf('%s%04.f/%s/%s/', cfg.recs, subj, cfg.vol.mod, 'raw'); % recording
+mdir = sprintf('%s%04.f/%s/%s/', cfg.data, subj, cfg.vol.mod, cfg.vol.cond); % mridata dir
+if ~isdir(mdir); mkdir(mdir); end
 
-rfile = sprintf('%s_%04.f_%s_%s', cfg.rec, subj, cfg.mod, cfg.cond); % recording
-dfile = sprintf('%s_%s_%04.f_%s_%s', cfg.proj, cfg.rec, subj, cfg.mod, cfg.cond); % data
+rfile = sprintf('%s_%04.f_%s_%s', cfg.rec, subj, cfg.vol.mod, cfg.vol.cond); % recording
+mfile = sprintf('%s_%04.f_%s_%s', cfg.rec, subj, cfg.vol.mod, cfg.vol.cond); % mridata
 
 ext = '.nii.gz';
 %---------------------------%
-
 
 if exist([rdir rfile ext], 'file')
   
   %---------------------------%
   %-get data
-  if ~exist([ddir dfile ext], 'file')
-    system(['ln ' rdir rfile ext ' ' ddir dfile ext]);
+  if ~exist([mdir mfile ext], 'file')
+    system(['ln ' rdir rfile ext ' ' mdir mfile ext]);
   end
   %---------------------------%
   
@@ -55,13 +55,13 @@ if exist([rdir rfile ext], 'file')
     %-realign
     %-------%
     %-bet
-    system(['bet ' ddir dfile ' ' ddir dfile '_brain -f 0.5 -g 0']);
+    system(['bet ' mdir mfile ' ' mdir mfile '_brain -f 0.5 -g 0']);
     %-------%
     
     %-------%
     %-flirt
-    system(['flirt -in ' ddir dfile '_brain -ref /usr/share/data/fsl-mni152-templates/MNI152_T1_1mm_brain.nii.gz ' ...
-      '-out ' ddir dfile '_brain_flirt -omat ' ddir dfile '_brain_flirt.mat ' ...
+    system(['flirt -in ' mdir mfile '_brain -ref /usr/share/data/fsl-mni152-templates/MNI152_T1_1mm_brain.nii.gz ' ...
+      '-out ' mdir mfile '_brain_flirt -omat ' mdir mfile '_brain_flirt.mat ' ...
       '-bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear']);
     %-------%
     
@@ -71,13 +71,13 @@ if exist([rdir rfile ext], 'file')
     
     %-------%
     %-apply flirt
-    system(['flirt -in ' ddir dfile ' -ref /usr/share/data/fsl-mni152-templates/MNI152_T1_1mm_brain.nii.gz ' ...
-      '-out ' ddir dfile '_' cfg.normalize ' -applyxfm -init ' ddir dfile '_brain_flirt.mat']);
+    system(['flirt -in ' mdir mfile ' -ref /usr/share/data/fsl-mni152-templates/MNI152_T1_1mm_brain.nii.gz ' ...
+      '-out ' mdir mfile '_' cfg.normalize ' -applyxfm -init ' mdir mfile '_brain_flirt.mat']);
     %-------%
     
     %-------%
     %-feedback
-    aff = dlmread([ddir dfile '_brain_flirt.mat']);
+    aff = dlmread([mdir mfile '_brain_flirt.mat']);
     
     outtmp = sprintf('flirt affine matrix:\n');
     for i = 1: (size(aff,1)-1)
@@ -88,7 +88,7 @@ if exist([rdir rfile ext], 'file')
     
     %-------%
     %-delete
-    delete([ddir '*_brain*'])
+    delete([mdir '*_brain*'])
     %-------%
     %---------------------------%
     %-----------------------------------------------%
@@ -100,12 +100,12 @@ if exist([rdir rfile ext], 'file')
     %-spm
     %---------------------------%
     %-defaults for SPM
-    refimg = '/data/toolbox/spm8/templates/T1.nii,1';
-    outsn = [ddir dfile '_sn.mat'];
+    refimg = [fullfile(fileparts(which('spm')), 'templates/T1.nii') ',1'];
+    outsn = [mdir mfile '_sn.mat'];
     %-----------------%
     %-unzip
-    gunzip([ddir dfile ext]);
-    mfile = [dfile ext(1:4)];
+    gunzip([mdir mfile ext]);
+    mfile = [mfile ext(1:4)];
     %-----------------%
     %---------------------------%
     
@@ -119,7 +119,7 @@ if exist([rdir rfile ext], 'file')
     eflags.nits = 16;
     eflags.reg = 1;
     
-    spm_normalise(refimg, [ddir mfile], outsn, '', '', eflags);
+    spm_normalise(refimg, [mdir mfile], outsn, '', '', eflags);
     %---------------------------%
     
     %---------------------------%
@@ -131,16 +131,16 @@ if exist([rdir rfile ext], 'file')
     rflags.interp = 1;
     rflags.wrap = [0 0 0];
     rflags.prefix = 'w';
-    spm_write_sn([ddir mfile], outsn, rflags);
+    spm_write_sn([mdir mfile], outsn, rflags);
     %---------------------------%
     
     %---------------------------%
     %-clean up
-    uncomp = [ddir dfile '_' cfg.normalize ext(1:4)];
-    system(['mv ' ddir rflags.prefix mfile ' ' uncomp]);
+    uncomp = [mdir mfile '_' cfg.normalize ext(1:4)];
+    system(['mv ' mdir rflags.prefix mfile ' ' uncomp]);
     gzip(uncomp);
     
-    delete([ddir mfile])
+    delete([mdir mfile])
     delete(uncomp);
     %---------------------------%
     %-----------------------------------------------%
@@ -150,7 +150,7 @@ if exist([rdir rfile ext], 'file')
   %---------------------------%
   %-copy data to main directory
   if ~isempty(cfg.smri)
-    system(['ln ' ddir dfile '_' cfg.normalize '.nii.gz ' cfg.smri dfile '_' cfg.normalize '.nii.gz']);
+    system(['ln ' mdir mfile '_' cfg.normalize '.nii.gz ' cfg.smri mfile '_' cfg.normalize '.nii.gz']);
   end
   %---------------------------%
   
@@ -165,8 +165,8 @@ end
 %---------------------------%
 %-end log
 toc_t = toc(tic_t);
-outtmp = sprintf('(p%02.f) %s (v%02.f) ended at %s on %s after %s\n\n', ...
-  subj, mfilename, mversion, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'), ...
+outtmp = sprintf('(p%02.f) %s ended at %s on %s after %s\n\n', ...
+  subj, mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'), ...
   datestr( datenum(0, 0, 0, 0, 0, toc_t), 'HH:MM:SS'));
 output = [output outtmp];
 
